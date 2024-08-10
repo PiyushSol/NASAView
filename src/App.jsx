82848a21dabb1ac3ec30d98c2f4edb090f97e13e
Piyush Solanki <solankipiyush596@gmail.com>
2024-08-10 23:38:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import SideBar from "./components/SideBar"
 function App() {
 
   const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(false)
+  // const [loading, setLoading] = useState(false)
   const [showModal , setShowModal] =useState(false);
 
   function handleToggleModal(){
